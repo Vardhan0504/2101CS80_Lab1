8939a231d82bb6fd1a01cb35236e54fe7a8d4de1
Vardhan Gacche <vardhangacche@gmail.com>
2022-07-27 23:25:31 +0530

    Added Merge Sort

--- Sorts.c
+++ Sorts.c
@@ -51,6 +51,65 @@ void bubbleSort(int arr[], int n)
     }
 }
 
+// Merge Sort
+void merge(int arr[], int l, int m, int r)
+{
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+    int L[n1], R[n2];
+
+    for (i = 0; i < n1; i++)
+    {
+        L[i] = arr[l + i];
+    }
+    for (j = 0; j < n2; j++)
+    {
+        R[j] = arr[m + 1 + j];
+    }
+
+    i = 0;
+    j = 0;
+    k = l;
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= R[j])
+        {
+            arr[k] = L[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+    while (i < n1)
+    {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+    while (j < n2)
+    {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(int arr[], int l, int r)
+{
+    if (l < r)
+    {
+        int m = l + (r - l) / 2;
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+        merge(arr, l, m, r);
+    }
+}
+
 int main()
 {
     int n;
@@ -64,6 +123,8 @@ int main()
     printf("Enter 1 for Insertion Sort\n");
     printf("Enter 2 for Selection Sort\n");
     printf("Enter 3 for Bubble Sort\n");
+    printf("Enter 4 for Merge Sort\n");
+
     scanf("%d", &x);
     switch (x)
     {
@@ -83,6 +144,13 @@ int main()
         printf("\n");
     case 3:
         bubbleSort(array, n);
+        for (int i = 0; i < n; i++)
+        {
+            printf("%d ", array[i]);
+        }
+        printf("\n");
+    case 4:
+        mergeSort(array, 0, n - 1);
         for (int i = 0; i < n; i++)
         {
             printf("%d ", array[i]);
