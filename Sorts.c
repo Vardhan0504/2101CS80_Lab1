7dd6c677005306e12880bbffd01b7a8bd46d2856
Vardhan Gacche <vardhangacche@gmail.com>
2022-07-27 23:10:30 +0530

    Added Selection Sort

--- Sorts.c
+++ Sorts.c
@@ -17,6 +17,24 @@ void insertionSort(int arr[], int n)
     }
 }
 
+// Selection Sort
+void selectionSort(int arr[], int n)
+{
+    int i, j, min_idx;
+    for (i = 0; i < n - 1; i++)
+    {
+        min_idx = i;
+        for (j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[min_idx])
+            {
+                min_idx = j;
+            }
+        }
+        swap(&arr[min_idx], &arr[i]);
+    }
+}
+
 int main()
 {
     int n;
@@ -28,11 +46,19 @@ int main()
     }
     int x;
     printf("Enter 1 for Insertion Sort\n");
+    printf("Enter 2 for Selection Sort\n");
     scanf("%d", &x);
     switch (x)
     {
     case 1:
         insertionSort(array, n);
+        for (int i = 0; i < n; i++)
+        {
+            printf("%d ", array[i]);
+        }
+        printf("\n");
+    case 2:
+        selectionSort(array, n);
         for (int i = 0; i < n; i++)
         {
             printf("%d ", array[i]);
